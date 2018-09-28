ca0bc3ea48dee17463d5fdbb2426a2cfbe5b0de8
Danny Morgan <dan@dannydev.com>
2018-09-27 22:25:03 -0400

    Rewrite almost everything

---
+++ cli/index.js
@@ -0,0 +1,19 @@
+const fs = require("fs-extra");
+const path = require("path");
+const yamlToJson = require("yamljs");
+const jsonToSass = require("json-sass");
+
+(async () => {
+  const asJson = yamlToJson.load("./config.yml");
+  const cwd = process.cwd();
+  const tempDir = path.join(cwd, ".temp");
+  const tempJson = path.join(tempDir, "config.json");
+  // const tempSass = path.join(tempDir, "config.scss");
+  const destSass = path.join(cwd, "src", "_custom.scss");
+  await fs.ensureDir(tempDir);
+  await fs.writeFile(tempJson, JSON.stringify(asJson, undefined, 2));
+  fs.createReadStream(tempJson)
+    .pipe(jsonToSass({ prefix: "$skeletor: " }))
+    .pipe(fs.createWriteStream(destSass));
+  // await fs.remove(tempDir);
+})();
