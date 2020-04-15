fca17823d6b694af4b64543e092c7180dc222a72
Dan Morgan <hello@danny.codes>
2020-04-15 18:55:09 -0400

    Start replacing scripts with node sass render

--- packages/css/cli/run.js
+++ packages/css/cli/run.js
@@ -3,54 +3,55 @@ const fs = require("fs-extra");
 const path = require("path");
 const yamlToJson = require("yamljs");
 const jsonToSass = require("json-sass");
-const spawn = require("cross-spawn");
+const sass = require("node-sass");
 
-function writeSassIndexByName(name, opts) {
-  const cmd = "node-sass-chokidar";
-  const dest = `./dist/sass/${name}.scss`;
-  const maps = ["--source-map", String(opts.flags.sourcemap)];
-  const compact = ["--output-style", "compact"];
-  const compressed = ["--output-style", "compressed"];
-  spawn.sync(
-    "yarn",
-    [cmd, dest, `./dist/${name}.css`, ...maps, ...compact],
-    opts.std
-  );
-  if (opts.flags.gzip) {
-    spawn.sync("gzip", ["-k", `./dist/${name}.css`], opts.std);
-  }
+async function writeSassIndexByName(name, opts) {
+  const src = path.join(__dirname, `../temp/sass/${name}.scss`);
+  const dest = path.join(__dirname, `../temp/${name}.css`);
+  // const maps = ["--source-map", String(opts.flags.sourcemap)];
+  const result = sass.renderSync({
+    file: src,
+    outFile: dest,
+    outputStyle: "compact",
+    sourceMap: true,
+  });
+  await fs.writeFile(dest, result.css);
+  // if (opts.flags.gzip) {
+  //   spawn.sync("gzip", ["-k", `./temp/${name}.css`], opts.std);
+  // }
 
-  if (opts.flags.minify) {
-    spawn.sync(
-      "yarn",
-      [cmd, dest, `./dist/${name}.min.css`, ...maps, ...compressed],
-      opts.std
-    );
+  // if (opts.flags.minify) {
+  // spawn.sync(
+  //   "yarn",
+  //   [cmd, dest, `./temp/${name}.min.css`, ...maps, ...compressed],
+  //   opts.std
+  // );
 
-    if (opts.flags.gzip) {
-      spawn.sync("gzip", ["-k", `./dist/${name}.min.css`], opts.std);
-    }
-  }
+  // if (opts.flags.gzip) {
+  //   spawn.sync("gzip", ["-k", `./temp/${name}.min.css`], opts.std);
+  // }
+  // }
 }
 
 async function main({ config, output, gzip, minify, sourcemap, sass } = {}) {
   const cwd = process.cwd();
   const local = path.join(__dirname, "..");
   const paths = {
-    config: path.join(cwd, config)
+    config: path.join(cwd, config),
   };
   const asJsonObject = yamlToJson.load(paths.config);
   const asJsonString = JSON.stringify(asJsonObject, undefined, 2);
   const srcDir = path.join(local, "src");
-  const destDir = path.join(local, "dist");
+  const destDir = path.join(local, "temp");
+  const srcSassDir = path.join(srcDir, "sass");
   const destSassDir = path.join(destDir, "sass");
   const tempJson = path.join(destDir, "config.json");
   const sassConfig = path.join(destSassDir, "_config.scss");
   await fs.remove(destDir);
-  await fs.copy(srcDir, destSassDir);
+  await fs.copy(srcSassDir, destSassDir);
   await fs.writeFile(tempJson, asJsonString);
 
-  await new Promise(resolve => {
+  await new Promise((resolve) => {
     fs.createReadStream(tempJson)
       .pipe(jsonToSass({ prefix: "$skeletor: " }))
       .pipe(fs.createWriteStream(sassConfig))
@@ -59,23 +60,23 @@ async function main({ config, output, gzip, minify, sourcemap, sass } = {}) {
 
   const opts = {
     flags: { gzip, minify, sourcemap },
-    std: { stdio: "inherit", cwd: local }
+    std: { stdio: "inherit", cwd: local },
   };
 
-  writeSassIndexByName("skeletor", opts);
-  writeSassIndexByName("skeletor.vars", opts);
+  await writeSassIndexByName("skeletor", opts);
+  await writeSassIndexByName("skeletor.vars", opts);
 
   if (sass) {
     // if we're copying over the Sass directory to cwd, we need to clean
     // up the non-core stuff which would produce duplicated rules
-    await fs.remove(path.join(destDir, "sass/custom-properties"));
-    await fs.remove(path.join(destDir, "sass/grid"));
-    await fs.remove(path.join(destDir, "sass/utilities"));
-    await fs.remove(path.join(destDir, "sass/skeletor.scss"));
-    await fs.remove(path.join(destDir, "sass/skeletor.vars.scss"));
+    await fs.remove(path.join(destSassDir, "custom-properties"));
+    await fs.remove(path.join(destSassDir, "grid"));
+    await fs.remove(path.join(destSassDir, "utilities"));
+    await fs.remove(path.join(destSassDir, "skeletor.scss"));
+    await fs.remove(path.join(destSassDir, "skeletor.vars.scss"));
   } else {
     // else kill it
-    await fs.remove(path.join(destDir, "sass"));
+    await fs.remove(destSassDir);
   }
 
   // now there's no rule-producing partials left, copy core for use
